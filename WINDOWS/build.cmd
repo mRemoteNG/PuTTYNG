77d81065d91e7c57dd6b3cf902b0f1c9515c3494
Kaim, Sean M <Sean.Kaim@ca.com>
2015-11-12 13:45:44 -0500

    Update VS 2015 build environment
    Update output file in VS builds
    Use VS command line build in build.cmd accordingly

--- WINDOWS/build.cmd
+++ WINDOWS/build.cmd
@@ -1,3 +1,2 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
-nmake -f MAKEFILE.NG clean
-nmake -f MAKEFILE.NG VER="/DNG_VER_MAJOR=0 /DNG_VER_MINOR=0 /DNG_VER_BUILD=0 /DNG_VER_REVISION=0"
+"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" "D:\Source\PuTTYNG\WINDOWS\VS2015\PuTTYNG.sln" /Rebuild Release
