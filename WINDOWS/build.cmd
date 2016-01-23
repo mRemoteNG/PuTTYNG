5e6c53bc52da673110293bbbbe27a0092c2564fc
Kaim, Sean M <Sean.Kaim@ca.com>
2016-01-23 14:25:27 -0500

    Build updates
    build.cmd doesn't seem to build PuTTYNG 100% correctly - get rid of it
    make.cmd to replace it
    minor project changes

--- WINDOWS/build.cmd
+++
@@ -1,2 +0,0 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
-"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" "D:\Source\PuTTYNG\WINDOWS\VS2015\PuTTYNG.sln" /Rebuild Release
