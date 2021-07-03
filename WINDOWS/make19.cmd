0af61d4162e3012474293e7cddbe04ba4b2d9226
kmscode <kmscode@users.noreply.github.com>
2021-07-03 16:57:17 -0400

    make19.cmd - build using VS 2019

---
+++ WINDOWS/make19.cmd
@@ -0,0 +1,7 @@
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat" (
+	call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
+	) else (
+	call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat"
+	)
+nmake -f MAKEFILE.NG clean
+nmake -f MAKEFILE.NG VER="/DNG_VER_MAJOR=0 /DNG_VER_MINOR=0 /DNG_VER_BUILD=0 /DNG_VER_REVISION=0"
