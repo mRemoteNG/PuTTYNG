dec46d1890de6d1c7a060ea53b4c25dc0854fec5
Dimitrij <kvarkas@gmail.com>
2022-01-21 21:28:50 +0000

    fix path

--- WINDOWS/make22.cmd
+++ WINDOWS/make22.cmd
@@ -2,20 +2,23 @@
 echo.
 echo Starting
 if exist "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat" (
-	call "C:\Program Files\Microsoft Visual Studio\\2022\Community\Common7\Tools\VsDevCmd.bat"
+	SET "_VSPATHBASE=C:\Program Files\Microsoft Visual Studio\2022\Community"
 ) 
 if exist "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\Tools\VsDevCmd.bat" (
-	call "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\Tools\VsDevCmd.bat"
+	SET "_VSPATHBASE=C:\Program Files\Microsoft Visual Studio\2022\Professional"
 )
 if exist "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat" (
-	call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat"
+	SET "_VSPATHBASE=C:\Program Files\Microsoft Visual Studio\2022\Professional"
 )
+
+echo.
+call "%_VSPATHBASE%\Common7\Tools\VsDevCmd.bat"
 echo.
 echo Cleaning...
-"C:\Program Files\Microsoft Visual Studio\2022\Professional\SDK\ScopeCppSDK\vc15\VC\bin\nmake.exe" -f MAKEFILE.NG clean
+"%_VSPATHBASE%\SDK\ScopeCppSDK\vc15\VC\bin\nmake.exe" -f MAKEFILE.NG clean
 echo.
 echo Building...
-"C:\Program Files\Microsoft Visual Studio\2022\Professional\SDK\ScopeCppSDK\vc15\VC\bin\nmake.exe" -f MAKEFILE.NG VER="/DNG_VER_MAJOR=0 /DNG_VER_MINOR=76 /DNG_VER_BUILD=0 /DNG_VER_REVISION=0"
+"%_VSPATHBASE%\SDK\ScopeCppSDK\vc15\VC\bin\nmake.exe" -f MAKEFILE.NG VER="/DNG_VER_MAJOR=0 /DNG_VER_MINOR=76 /DNG_VER_BUILD=0 /DNG_VER_REVISION=0"
 echo.
 echo Finished
 echo.
