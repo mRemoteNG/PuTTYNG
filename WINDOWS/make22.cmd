304204b2788420b99b4cb9a65d17a30d67e3f2a5
Dimitrij <kvarkas@gmail.com>
2022-10-30 18:07:22 +0000

    small adjustment

--- WINDOWS/make22.cmd
+++ WINDOWS/make22.cmd
@@ -8,20 +8,22 @@ if exist "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\Too
 	SET "_VSPATHBASE=C:\Program Files\Microsoft Visual Studio\2022\Professional"
 )
 if exist "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat" (
-	SET "_VSPATHBASE=C:\Program Files\Microsoft Visual Studio\2022\Professional"
+	SET "_VSPATHBASE=C:\Program Files\Microsoft Visual Studio\2022\Enterprise"
 )
 
 echo.
 call "%_VSPATHBASE%\Common7\Tools\VsDevCmd.bat"
 echo.
+call "%_VSPATHBASE%\Common7\Tools\vsdevcmd\ext\vcvars.bat"
+echo.
 echo Cleaning...
 "%_VSPATHBASE%\SDK\ScopeCppSDK\vc15\VC\bin\nmake.exe" -f MAKEFILE.NG clean
 echo.
 echo Building...
-"%_VSPATHBASE%\SDK\ScopeCppSDK\vc15\VC\bin\nmake.exe" -f MAKEFILE.NG VER="/DNG_VER_MAJOR=0 /DNG_VER_MINOR=76 /DNG_VER_BUILD=0 /DNG_VER_REVISION=0"
+"%_VSPATHBASE%\SDK\ScopeCppSDK\vc15\VC\bin\nmake.exe" -f MAKEFILE.NG VER="/DNG_VER_MAJOR=0 /DNG_VER_MINOR=78 /DNG_VER_BUILD=0 /DNG_VER_REVISION=0"
 echo.
 echo Set version
-start /b "" "C:\projects\puttyng-publish\rcedit-x64.exe" %~dp0PuTTYNG.exe --set-file-version "0.76.0" --set-product-version "4 RemoteNG"
+start /b "" "..\rcedit-x64.exe" %~dp0PuTTYNG.exe --set-file-version "0.78.0" --set-product-version "4 RemoteNG"
 echo.
 echo Finished
 echo.
