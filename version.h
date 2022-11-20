ff8760379c98d01d10f7285fe8d30c4cff35ae62
kmscode <sean.kaim@gmail.com>
2022-11-20 02:20:26 -0500

    build environment updates

--- version.h
+++ version.h
@@ -8,6 +8,6 @@
  * default stuff used for local development runs of 'make'.
  */
 
-#define TEXTVER "Unidentified build"
-#define SSHVER "-Unidentified-Local-Build"
-#define BINARY_VERSION 0,0,0,0
+#define TEXTVER "PuTTYNG"
+#define SSHVER "PuTTYNG"
+#define BINARY_VERSION 0,78,0,0
