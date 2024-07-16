dd4395133fcd3d970b4c7f7d6d26a45d1f2d5370
Dimitrij <kvarkas@gmail.com>
2024-07-16 13:31:49 +0100

    #243: Suppress pop-up windows creation in case of error or fatal error

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+## [0.81.0.1]
+
+### Updated
+- #243: Suppress pop-up windows creation in case of error or fatal error
--- PuTTYNG.ps1
+++ PuTTYNG.ps1
@@ -84,14 +84,6 @@ cd putty
 $getLastTag = git.exe describe --tags --match="*.*" --abbrev=0 HEAD 2>&1
 cd ..
 
-$workFile = "$workFolder\version.h"
-$setNewVersion = $getLastTag.split(".")[0] + "," + $getLastTag.split(".")[1] + ",0,0"
-write-host "Version: " $setNewVersion
-
-#Change version data
-(Get-Content $workFile).Replace('Unidentified build', 'Release '+ $getLastTag + ' mRemoteNG') | Set-Content $workFile
-(Get-Content $workFile).Replace('-Unidentified-Local-Build', '-Release-mRemoteNG-Build') | Set-Content $workFile
-(Get-Content $workFile).Replace('0,0,0,0', $setNewVersion) | Set-Content $workFile
 
 #Add mRemoteNG required changes
 $workFile = "$workFolder\cmdline.c"
@@ -116,6 +108,93 @@ int hwnd_parent;
 #define IsZoomed(hWnd) TRUE
 #endif // PUTTYNG'
 (Get-Content $workFile).Replace('extern const char *const appname;', $newContent) | Set-Content $workFile
+#===================================================================================================
+
+#Add mRemoteNG required changes
+$workFile = "$workFolder\version.h"
+$setNewVersion = $getLastTag.split(".")[0] + "," + $getLastTag.split(".")[1] + ",0,0"
+write-host "Version: " $setNewVersion
+
+#Change version data
+(Get-Content $workFile).Replace('Unidentified build', 'Release '+ $getLastTag + ' mRemoteNG') | Set-Content $workFile
+(Get-Content $workFile).Replace('-Unidentified-Local-Build', '-Release-mRemoteNG-Build') | Set-Content $workFile
+(Get-Content $workFile).Replace('0,0,0,0', $setNewVersion) | Set-Content $workFile
+#===================================================================================================
+
+#Add mRemoteNG required changes
+[string]$workFile = "$workFolder\windows\window.c"
+[string]$existingBlockStart = 'char *title = dupprintf("%s Fatal Error", appname);'
+[string]$existingBlockEnd = 'if (conf_get_int(wgs->conf, CONF_close_on_exit) == FORCE_ON)' 
+[string]$newBlock  = '    win_seat_output(0, 0, "\r\n------------------------------------------------------------------------\r\n", 76);
+    win_seat_output(0, 0, "Fatal Error: ", 2);
+    win_seat_output(0, 0, "Error Message", 13);
+    win_seat_output(0, 0, "\r\n", 2);
+    win_seat_output(0, 0, "\r\n------------------------------------------------------------------------\r\n", 76);
+    //show_mouseptr(wgs, true);
+    //MessageBox(wgs->term_hwnd, msg, title, MB_ICONERROR | MB_OK);
+    sfree(title);
+
+    '
+
+# Read the content of the file
+$fileContent = Get-Content -Path $workFile -Raw
+# Find the position of the existing block start
+$startIndex = $fileContent.IndexOf($existingBlockStart) + $existingBlockStart.Length + 1;
+
+if ($startIndex -ge 0) {
+    # Find the end of the existing block (e.g., next empty line or closing brace)
+    $endIndex = $fileContent.IndexOf($existingBlockEnd, $startIndex)
+
+    # Remove the existing block
+    $modifiedContent = $fileContent.Remove($startIndex, $endIndex - $startIndex)
+
+    # Insert the new block after the existing block start
+    $modifiedContent = $modifiedContent.Insert($startIndex, $newBlock)
+
+    # Write the modified content back to the file
+    Set-Content -Path $workFile -Value $modifiedContent
+
+    Write-Host "Code block replaced successfully in '$workFile'."
+} else {
+    Write-Host "Existing block start not found in the file." -ForegroundColor Red
+}
+#===================================================================================================
+
+#Add mRemoteNG required changes
+[string]$workFile = "$workFolder\windows\window.c"
+[string]$existingBlockStart = 'char *title = dupprintf("%s Error", appname);'
+[string]$existingBlockEnd = 'sfree(title);' 
+[string]$newBlock  = '    win_seat_output(0, 0, "\r\n------------------------------------------------------------------------\r\n", 76);
+    win_seat_output(0, 0, "Error Message", 13);
+    win_seat_output(0, 0, "\r\n", 2);
+    win_seat_output(0, 0, "\r\n------------------------------------------------------------------------\r\n", 76);
+    //show_mouseptr(wgs, true);
+    //MessageBox(wgs->term_hwnd, msg, title, MB_ICONERROR | MB_OK);
+    '
+
+# Read the content of the file
+$fileContent = Get-Content -Path $workFile -Raw
+# Find the position of the existing block start
+$startIndex = $fileContent.IndexOf($existingBlockStart) + $existingBlockStart.Length + 1;
+
+if ($startIndex -ge 0) {
+    # Find the end of the existing block (e.g., next empty line or closing brace)
+    $endIndex = $fileContent.IndexOf($existingBlockEnd, $startIndex)
+
+    # Remove the existing block
+    $modifiedContent = $fileContent.Remove($startIndex, $endIndex - $startIndex)
+
+    # Insert the new block after the existing block start
+    $modifiedContent = $modifiedContent.Insert($startIndex, $newBlock)
+
+    # Write the modified content back to the file
+    Set-Content -Path $workFile -Value $modifiedContent
+
+    Write-Host "Code block replaced successfully in '$workFile'."
+} else {
+    Write-Host "Existing block start not found in the file." -ForegroundColor Red
+}
+#===================================================================================================
 
 # run cmake
 try { 
